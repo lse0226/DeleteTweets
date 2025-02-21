48e7ba344f7a56e971f2ca5829ca9bf1b216a0ba
lse0226 <sen9295@naver.com>
2025-02-21 17:41:51 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-var authorization = "Bearer ***"; // replace by authorization value
+var authorization = "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"; // replace by authorization value
 var ua = navigator.userAgentData.brands.map(brand => `"${brand.brand}";v="${brand.version}"`).join(', ');
-var client_tid = "***"; // replace by X-Client-Transaction-Id value
-var client_uuid = "***"; // replace by X-Client-Uuid value
+var client_tid = "
+JfkZ98OZUP0Uf4OKWNhUy1/9O9We6JQTKHwqqjWQnV7gGZIOiRvHRNqrGWIBrMQ+1PvzTSb80lcaOnk5ecwqszetx7ieJg"; // replace by X-Client-Transaction-Id value
+var client_uuid = "
+a97155f9-9a46-4b76-b001-e80769866a41"; // replace by X-Client-Uuid value
 var csrf_token = getCookie("ct0");
 var random_resource = "uYU5M2i12UhDvDTzN6hZPg";
 var random_resource_old_tweets = "H8OOoI-5ZE4NxgRr8lfyWg"
 var language_code = navigator.language.split("-")[0]
 var tweets_to_delete = []
 var user_id = getCookie("twid").substring(4);
-var username = "YourUsernameHere" // replace with your username
+var username = "kkiiwyq" // replace with your username
 var stop_signal = undefined
 var twitter_archive_content = undefined
 var twitter_archive_loading_confirmed = false
